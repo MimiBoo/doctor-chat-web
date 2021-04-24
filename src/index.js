b6cbae909e65e754bc1460e469c07e72fe0d51a4
Melkia Mohamed <melkia02@gmail.com>
2021-04-24 23:33:36 +0100

    new build

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import firebase from 'firebase';
-import { Provider } from 'react-redux';
-import store from './store';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase";
+import { Provider } from "react-redux";
+import store from "./store";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBF8co3-j6lq7qGI9b8s0U1YYEIwqXb4_0",
-  authDomain: "web-messenger-4eb99.firebaseapp.com",
-  databaseURL: "https://web-messenger-4eb99.firebaseio.com",
-  projectId: "web-messenger-4eb99",
-  storageBucket: "web-messenger-4eb99.appspot.com",
-  messagingSenderId: "252569616509",
-  appId: "1:252569616509:web:2bbb4c394e9d608f147114",
-  measurementId: "G-VXYE76S2G7"
+  apiKey: "AIzaSyA2sQcYCW6qpaCioNGiDsvpP8f2fAjLsWQ",
+  authDomain: "doctor-chat-468af.firebaseapp.com",
+  projectId: "doctor-chat-468af",
+  storageBucket: "doctor-chat-468af.appspot.com",
+  messagingSenderId: "27725015947",
+  appId: "1:27725015947:web:cf592f027251e077119d8e",
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
@@ -31,7 +27,7 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
